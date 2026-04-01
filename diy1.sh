610a2cf51d3747084e54512ecb6441302445f037
cs2021hv001 <89976757+cs2021hv001@users.noreply.github.com>
2026-04-01 16:19:10 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git-full small https://github.com/kenzok8/jell.git' feeds.conf.default
 sed -i '$a src-git-full kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git-full xiaorouji_passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 #sed -i '$a src-git-full sbwml_mosdns https://github.com/sbwml/luci-app-mosdns.git' feeds.conf.default
