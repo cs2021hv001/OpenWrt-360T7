e6a2e9485889d7098190c7612e2ed65f1a1d4873
cs2021hv001 <89976757+cs2021hv001@users.noreply.github.com>
2026-04-02 19:32:02 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -27,9 +27,19 @@ sed -i 's/192.168.1.1/192.168.88.7/g' ppackage/base-files/luci2/bin/config_gener
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By 神棍 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
 
 # 修改内存为 512M
-sed -i 's/0x10000000/0x20000000/' target/linux/mediatek/dts/mt7981b-360-t7.dts
+#sed -i 's/0x10000000/0x20000000/' target/linux/mediatek/dts/mt7981b-360-t7.dts
 
 # 打印修改后的内容，日志里能直接看到
-echo "===== 检查 DTS 内存配置 ====="
-grep -E "0x[0-9a-fA-F]+$" target/linux/mediatek/dts/mt7981b-360-t7.dts
-echo "============================"
+#echo "===== 检查 DTS 内存配置 ====="
+#grep -E "0x[0-9a-fA-F]+$" target/linux/mediatek/dts/mt7981b-360-t7.dts
+#echo "============================"
+# 360T7 改 512MB 内存（适配你当前这个极简DTS）
+DTS_FILE="target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7.dts"
+
+# 256MB → 512MB
+sed -i 's|reg = <0 0x40000000 0 0x10000000>;|reg = <0 0x40000000 0 0x20000000>;|g' $DTS_FILE
+
+# 输出确认修改成功
+echo "===== 360T7 内存修改结果 ====="
+grep "memory" $DTS_FILE -A 1
+
