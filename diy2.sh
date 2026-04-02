6f3924166c3670333f49e0bbb9a0fa3bb747998d
cs2021hv001 <89976757+cs2021hv001@users.noreply.github.com>
2026-04-02 20:19:30 +0800

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -34,3 +34,15 @@ sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By 神棍 $(date +
 #grep -E "0x[0-9a-fA-F]+$" target/linux/mediatek/dts/mt7981b-360-t7.dts
 #echo "============================"
 
+# 进入目录（必须加）
+cd openwrt
+
+# 精准修改 360T7 DTS 内存为 512M
+DTS="target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-360-t7.dts"
+
+# 只改数字，不管空格格式，永不报错
+sed -i 's/0x10000000/0x20000000/' "$DTS"
+
+# 输出查看，防止编译中断
+echo "==== DTS memory 修改结果 ===="
+grep -A 1 -B 1 "reg.*0x" "$DTS" || true
